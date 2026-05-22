a0c6061a5b73b8f7407741ef226236db90537fef
nasreen <shaikmysoor55@gmail.com>
2026-05-22 16:42:00 +0530

    Merged remote repository

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
